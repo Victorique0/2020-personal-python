d1e927cdadd5313ac4a8dd0b34c34ba9e398fc0b
Victorique <65145969+Victorique0@users.noreply.github.com>
2020-09-13 23:57:13 +0000

    Update GHAnalysis.py

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -27,7 +27,6 @@ class Date:
                             self.add_user_repo_event(i, user_repo_event)
                     with open("./ver2.json", "a") as f_json:
                         json.dump(user_repo_event, f_json)
-
     def add_user_repo_event(self, dic, user_repo_event):
         id = dic["actor"]["login"]
         repo = dic["repo"]["name"]
